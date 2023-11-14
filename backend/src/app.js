9304af28741a7d6593f2bb69cc372a80c9a04f9a
Uriel Campos <urieldecampos@gmail.com>
2023-11-14 15:58:31 -0300

    adding cors in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import testRouter from './routes/test.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", testRouter);
 
