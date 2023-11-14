2c7d5db6faf9c097827030227a95f4bca0850f64
Uriel Campos <urieldecampos@gmail.com>
2023-11-14 15:19:33 -0300

    trying to configure webpack for esm

---
+++ backend/webpack.config.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    mode: 'production',
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'finals.js',
+    },
+    target: 'node',
+};
